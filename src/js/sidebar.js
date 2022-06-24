eb3c645c22ed5d659870f6c041882dc91979d28c
bahtiyar-85 <b.ilyazov@gmail.com>
2022-06-24 14:40:56 +0600

    fix sidebar bug

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,6 +1,7 @@
 
 const sidebarOpenBtn = document.querySelector(".header__burger")
 const sidebarCloseBtn = document.querySelector(".sidebar__btn_close")
+const linkClose = document.querySelector(".sidebar__link_close")
 
 export const showHideToggle = (className) => {
     const link = document.querySelector(`.${className}`)
@@ -10,4 +11,5 @@ export const showHideToggle = (className) => {
 export const sidebarInit = () => {
     sidebarOpenBtn.addEventListener('click', () => showHideToggle('sidebar'))
     sidebarCloseBtn.addEventListener('click', () => showHideToggle('sidebar'))
+    linkClose.addEventListener('click', () => showHideToggle('sidebar'))
 }
